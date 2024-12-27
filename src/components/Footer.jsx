3a9a0547367919c8564ce3c79e76376959a01033
agrim08 <agrimgupta0805@gmail.com>
2024-12-27 19:48:30 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="mt-20 border border-stone-50/10 p-5 w-full rounded-xl mx-2 md:flex md:justify-between md:items-center">
       <div className="flex items-center justify-center">
-        <div className="text-4xl text-white ">AGRIM GUPTA</div>
+        <div className="text-4xl text-white sm:hidden">AGRIM GUPTA</div>
       </div>
       <div className="flex items-center justify-center gap-8 mt-0 xsm:mt-8 md:mt-0">
         {SOCIAL_MEDIA_LINKS.map((link, index) => (
