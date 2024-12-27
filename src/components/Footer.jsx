7e4881faa2ff504042b018e85194fb90ab4e917f
agrim08 <agrimgupta0805@gmail.com>
2024-12-27 20:01:29 +0530

    added framer motion animations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,25 @@
 import { SOCIAL_MEDIA_LINKS } from "../constants";
+import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
-    <div className="mt-20 border border-stone-50/10 p-5 w-full rounded-xl mx-2 md:flex md:justify-between md:items-center">
+    <div className="mt-20  p-5 w-full rounded-xl mx-2 md:flex md:justify-between md:items-center">
       <div className="flex items-center justify-center">
         <div className="text-4xl text-white hidden md:flex">AGRIM GUPTA</div>
       </div>
       <div className="flex items-center justify-center gap-8 mt-0 xsm:mt-8 md:mt-0">
         {SOCIAL_MEDIA_LINKS.map((link, index) => (
-          <a
+          <motion.a
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.2, delay: index * 0.5 }}
             key={index}
             href={link.href}
             target="blank"
             rel="noopener noreferrer"
           >
             {link.icon}
-          </a>
+          </motion.a>
         ))}
       </div>
     </div>
